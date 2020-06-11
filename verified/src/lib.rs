80bf59c4f8e4e2d9a0da849acc69114c0254ae62
Bob Gebert <bobgebert7@gmail.com>
2020-06-10 20:07:12 -0700

    Update documentation

--- verified/src/lib.rs
+++ verified/src/lib.rs
@@ -21,14 +21,18 @@
 //! fn slow_routine<E, Size: Unsigned>(working_set: Collection<E, Size>)
 //! where
 //!     // Restrict the size of the working set.
-//!     _: Verify<{ Size < 128 }>
+//!     _: Verify<{ Size < 128 }, { Size > 0 }>
 //! {
 //!     // TODO
 //! }
 //!
 //! fn main() {
 //!     // No problem here...
+//!     slow_routine::<String, U1>(Default::default());
 //!     slow_routine::<String, U127>(Default::default());
+//!
+//!     // XXX: Does not compile because our working set is empty.
+//!     slow_routine::<String, U0>(Default::default());
 //!
 //!     // XXX: Does not compile because our working set is one element too large.
 //!     slow_routine::<String, U128>(Default::default());
