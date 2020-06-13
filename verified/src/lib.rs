4b7f82219e2d2414df9a0903326fca4308d8db5d
Bob Gebert <bobgebert7@gmail.com>
2020-06-12 21:48:29 -0700

    Bump version

--- verified/src/lib.rs
+++ verified/src/lib.rs
@@ -38,6 +38,71 @@
 //!     slow_routine::<String, U128>(Default::default());
 //! }
 //! ```
+//!
+//! For a more complete example, see the `vec` module. Here is an abbreviated snippet:
+//!
+//! ```
+//! use verified::*;
+//! use std::vec::Vec as Raw;
+//!
+//! pub struct Vec<Size: Unsigned, Element>(Size, Raw<Element>);
+//!
+//! #[verify]
+//! impl<Size: Unsigned, Element> Vec<Size, Element> {
+//!     pub fn append<OtherSize: Unsigned>(
+//!         self,
+//!         other: Vec<OtherSize, Element>,
+//!     ) -> Vec<{ Size + OtherSize }, Element> {
+//!         self + other
+//!     }
+//!
+//!     pub fn pop(self) -> (Vec<{ Size - 1 }, Element>, Element)
+//!     where
+//!         _: Verify<{ Size > 0 }>,
+//!     {
+//!         self.into()
+//!     }
+//!
+//!     pub fn push(self, e: Element) -> Vec<{ Size + 1 }, Element> {
+//!         (self, e).into()
+//!     }
+//! }
+//!
+//! #[verify]
+//! impl<SizeL: Unsigned, SizeR: Unsigned, Element> std::ops::Add<Vec<SizeR, Element>>
+//!     for Vec<SizeL, Element>
+//! {
+//!     type Output = Vec<{ SizeL + SizeR }, Element>;
+//!     fn add(self, Vec(os, mut ov): Vec<SizeR, Element>) -> Self::Output {
+//!         let Self(s, mut v) = self;
+//!         v.append(&mut ov);
+//!         Vec(s + os, v)
+//!     }
+//! }
+//!
+//! #[verify]
+//! impl<Size: Unsigned, Element> std::convert::From<(Vec<Size, Element>, Element)>
+//!     for Vec<{ Size + 1 }, Element>
+//! {
+//!     fn from((Vec(_, mut v), e): (Vec<Size, Element>, Element)) -> Self {
+//!         v.push(e);
+//!         Self(Default::default(), v)
+//!     }
+//! }
+//!
+//! #[verify]
+//! impl<Size: Unsigned, Element> std::convert::From<Vec<Size, Element>>
+//!     for (Vec<{ Size - 1 }, Element>, Element)
+//! where
+//!     _: Verify<{ Size > 0 }>,
+//! {
+//!     fn from(Vec(_, mut v): Vec<Size, Element>) -> Self {
+//!         let e = v.pop().unwrap();
+//!         (Vec(Default::default(), v), e)
+//!     }
+//! }
+//! ```
+
 pub mod vec;
 pub use std::ops::{Add, BitAnd, BitOr, BitXor, Div, Mul, Not, Rem, Shl, Shr, Sub};
 pub use typenum::*;
